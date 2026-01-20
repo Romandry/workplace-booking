b02236298e2dd206b78b8c58499af18b18c649be
Andriy <transkyy@gmail.com>
2026-01-21 00:33:54 +0200

    CI fix

---
+++ .php-cs-fixer.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests');
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'declare_strict_types' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'single_quote' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => true,
+        'phpdoc_align' => false,
+    ])
+    ->setFinder($finder);
